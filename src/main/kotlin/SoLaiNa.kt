f51a211e9766dff53512b16a9e24547299f60394
RoselynOwl <126867024+RoselynOwl@users.noreply.github.com>
2025-03-06 13:23:36 +0800

    Add self-introduction feature

---
+++ src/main/kotlin/SoLaiNa.kt
@@ -0,0 +1,10 @@
+package hk.edu.hkbu.comp.comp4107.playground.SoLaiNa
+
+class SelfIntroduction {
+    fun selfIntroduction() {
+        println("Hello, my name is So Lai Na.")
+        println("I am a student at HKBU.")
+        println("I am studying Computer Science.")
+        println("I am interested in machine learning.")
+    }
+}
